2ebb903d7102477d633f9866c5b7935f4e83b9c2
Alfian Aldy Hamdani <alfian.hamdani04@gmail.com>
2022-11-02 16:51:58 +0800

    Fix responsive checkbox and clean kurikulum ID

--- kurikulum/forms.py
+++ kurikulum/forms.py
@@ -6,7 +6,6 @@ from .utils import get_kurikulum_by_prodi
 
 class KurikulumReadAllSyncForm(forms.Form):
     kurikulum_from_neosia = forms.MultipleChoiceField(
-        # widget=forms.CheckboxSelectMultiple,
         widget=ChoiceListInteractive(
             badge_template='kurikulum/partials/badge-list-kurikulum.html',
             list_custom_field_template='kurikulum/partials/list-custom-field-kurikulum.html',
@@ -23,10 +22,22 @@ class KurikulumReadAllSyncForm(forms.Form):
         self.label_suffix = ""
         
         kurikulum_choices = get_kurikulum_by_prodi(self.user.prodi.id_neosia)
-        print(self.fields)
+        
         self.fields['kurikulum_from_neosia'].choices = kurikulum_choices
     
     def clean(self):
         cleaned_data = super().clean()
+
+        # Clean kurikulum IDs
+        kurikulum_from_neosia = cleaned_data.get('kurikulum_from_neosia')
+        new_id_kurikulum_from_neosia = []
+        
+        for id in kurikulum_from_neosia:
+            if id in new_id_kurikulum_from_neosia: continue
+            new_id_kurikulum_from_neosia.append(id)
+        
+        cleaned_data['kurikulum_from_neosia'] = new_id_kurikulum_from_neosia
+
         if settings.DEBUG: print("Clean data: {}".format(cleaned_data))
+        
         return cleaned_data
