6211f292ad97426e4190420b56aa2c8c61ab909a
Alfian Aldy Hamdani <alfian.hamdani04@gmail.com>
2022-11-03 15:40:27 +0800

    Implement OAuth for authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from .views import (
     login_view,
-    logout_view
+    logout_view,
+    oauth_callback,
 )
 
-app_name = "accounts"
+app_name = 'accounts'
 urlpatterns = [
-    path("login/", login_view, name="login"),
-    path("logout/", logout_view, name="logout"),
+    path('login/', login_view, name='login'),
+    path('login/oauth/callback', oauth_callback, name='oauth-callback'),
+    path('logout/', logout_view, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,58 @@
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
 from django.contrib.auth import login, logout
 from django.shortcuts import redirect, render
-from .forms import MyAuthForm
+from django.urls import reverse
+from urllib.parse import urlencode
+import requests
+from requests import Request, Session
+import os
 
 
 # Create your views here.
 def login_view(request: HttpRequest):
-    form = MyAuthForm(request, data=request.POST or None)
+    redirect_uri = '127.0.0.1:8000' + reverse('accounts:oauth-callback')
+    parameters = {
+        'client_id': '3',
+        'redirect_uri':'http://{}'.format(redirect_uri),
+        'response_type': 'code',
+        'scope': '*',
+    }
 
-    if form.is_valid():
-        login(request, user=form.get_user())
+    print(parameters)
+    # return HttpResponse()
+    return redirect('https://mberkas.unhas.ac.id/oauth/authorize?{}'.format(urlencode(parameters)))
 
-        return redirect("/")
+def oauth_callback(request: HttpRequest):
+    code = request.GET['code']
+    redirect_uri = '127.0.0.1:8000' + reverse('accounts:oauth-callback')
+    parameters = {
+        'grant_type': 'authorization_code',
+        'client_id': '3',
+        'client_secret': os.environ.get('OAUTH_CLIENT_SECRET'),
+        'redirect_uri': 'http://{}'.format(redirect_uri),
+        'code': code,
+    }
     
-    return render(request, "accounts/login.html", context={"form": form})
+    req = Request(
+        'POST',
+        'https://mberkas.unhas.ac.id/oauth/token',
+        files={
+            'grant_type': (None, parameters['grant_type']),
+            'client_id': (None, parameters['client_id']),
+            'client_secret': (None, parameters['client_secret']),
+            'redirect_uri': (None, parameters['redirect_uri']),
+            'code': (None, code),
+        }
+    ).prepare()
+    s = Session()
+    response = s.send(req)
+
+    print("Response Status: {}".format(response.status_code))
+    print("Response: {}".format(response.json()))
+    
+    return HttpResponse()
 
 def logout_view(request: HttpRequest):
     logout(request)
-    return redirect("/")
+    return redirect('/')
     
