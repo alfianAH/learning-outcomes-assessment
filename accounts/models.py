2c31b40c96bbfe03adbc5cd3d272e8a14004d33b
Alfian Aldy Hamdani <alfian.hamdani04@gmail.com>
2022-11-03 19:54:18 +0800

    Move managers to models.py and delete auth forms

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from .managers import UserOAuthManager
+from django.contrib.auth.models import AbstractUser, Group
 from .enums import RoleChoices
+
 
 # Create your models here.
 class Fakultas(models.Model):
@@ -11,6 +11,7 @@ class Fakultas(models.Model):
     def __str__(self) -> str:
         return self.nama
 
+
 class ProgramStudi(models.Model):
     fakultas = models.ForeignKey(Fakultas, on_delete=models.CASCADE)
     id_neosia = models.IntegerField(unique=True, null=False, primary_key=True)
@@ -18,6 +19,55 @@ class ProgramStudi(models.Model):
 
     def __str__(self) -> str:
         return self.nama
+
+
+class UserOAuthManager(models.UserManager):
+    def create_admin_prodi_user(self, user: dict, prodi: ProgramStudi):
+        admin_prodi_group, _ = Group.objects.get_or_create(name='Admin Program Studi')
+        new_user = self.create_user(
+            user['nip'],
+            user['nama_admin'],
+            RoleChoices.ADMIN_PRODI,
+            prodi
+        )
+
+        new_user.groups.add(admin_prodi_group)
+        return new_user
+
+    def create_dosen_user(self, user: dict, prodi: ProgramStudi):
+        dosen_group, _ = Group.objects.get_or_create(name='Dosen')
+        new_user = self.create_user(
+            user['nip'],
+            user['nama_dosen'],
+            RoleChoices.DOSEN,
+            prodi
+        )
+
+        new_user.groups.add(dosen_group)
+        return new_user
+    
+    def create_mahasiswa_user(self, user: dict, prodi: ProgramStudi):
+        mahasiswa_group, _ = Group.objects.get_or_create(name='Mahasiswa')
+        new_user = self.create_user(
+            user['nim'],
+            user['nama_mahasiswa'],
+            RoleChoices.MAHASISWA,
+            prodi
+        )
+
+        new_user.groups.add(mahasiswa_group)
+        return new_user
+    
+    def create_user(self, username: str, name: str, role: str, prodi: ProgramStudi):
+        new_user = self.create(
+            username = username,
+            name = name,
+            prodi = prodi,
+            role = role,
+        )
+        
+        return new_user
+
 
 class MyUser(AbstractUser):
     # Unused default fields
