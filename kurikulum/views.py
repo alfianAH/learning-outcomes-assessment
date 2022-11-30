2c4dd2658378da33723b3927346e0b19f96f54ae
Alfian Aldy Hamdani <alfian.hamdani04@gmail.com>
2022-11-30 10:54:09 +0800

    Make my own session wizard view

--- kurikulum/views.py
+++ kurikulum/views.py
@@ -8,7 +8,7 @@ from django.views.generic.base import View
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import DeleteView, FormView
 from django.views.generic.list import ListView
-from formtools.wizard.views import SessionWizardView
+from learning_outcomes_assessment.wizard.views import MySessionWizardView
 
 from accounts.models import ProgramStudi
 from semester.filters import SemesterFilter, SemesterSort
@@ -45,63 +45,18 @@ from semester.utils import(
 
 
 # Create your views here.
-class KurikulumReadAllSyncFormWizardView(SessionWizardView):
+class KurikulumReadAllSyncFormWizardView(MySessionWizardView):
     template_name: str = 'kurikulum/read-all-sync-form.html'
     form_list: list = [KurikulumFromNeosia, SemesterFromNeosia]
-    latest_page: str = '0'
-    revealed_page: list = []
 
     def get_form_kwargs(self, step=None):
         form_kwargs = super().get_form_kwargs(step)
-        if step is None: step = self.steps.current
-        self.update_latest_page(step)
-        self.update_revealed_page(step)
+        
         if step == '0' or step == '2':
             form_kwargs.update({'user': self.request.user})
         
         return form_kwargs
 
-    def get_context_data(self, form, **kwargs):
-        context = super().get_context_data(form, **kwargs)
-        extra_data = self.storage.extra_data
-        
-        context['latest_page'] = extra_data.get('latest_page')
-        context['revealed_page'] = extra_data.get('revealed_page')
-        return context
-
-    def update_latest_page(self, new_page: str):
-        # Get latest page from storage
-        extra_data = self.storage.extra_data
-        self.latest_page = extra_data.get('latest_page')
-        # Extra data will be none at the first time
-        if self.latest_page is None: 
-            # Set default value
-            self.latest_page = '0'
-        # Return if new page is smaller than latest page
-        if int(new_page) < int(self.latest_page): return
-
-        # Update latest page
-        self.latest_page = new_page
-        self.storage.extra_data.update({'latest_page': self.latest_page})
-
-    def update_revealed_page(self, new_page: str):
-        # Get latest page from storage
-        extra_data = self.storage.extra_data
-        revealed_page = extra_data.get('revealed_page')
-        # Extra data will be none at the first time
-        if revealed_page is None:
-            # Set default value
-            self.revealed_page = []
-        else:
-            # Load JSON value if there is extra data
-            self.revealed_page = json.loads(revealed_page)
-        # Return if new page is already in revealed page
-        if new_page in self.revealed_page: return
-        
-        # Update revealed page
-        self.revealed_page.append(new_page)
-        self.storage.extra_data.update({'revealed_page': json.dumps(self.revealed_page)})
-    
     def render_next_step(self, form, **kwargs):
         next_step = self.steps.next
         
@@ -238,11 +193,6 @@ class KurikulumReadAllSyncFormWizardView(SessionWizardView):
 
         semester_kurikulum_obj.save()
 
-    def update_kurikulum(self, kurikulum_id: int):
-        kurikulum_obj = Kurikulum.objects.filter(id_neosia=kurikulum_id)
-        new_kurikulum_data = get_detail_kurikulum(kurikulum_id)
-        kurikulum_obj.update(**new_kurikulum_data)
-
     def done(self, form_list, **kwargs):
         cleaned_data = self.get_all_cleaned_data()
         
@@ -324,7 +274,7 @@ class KurikulumBulkUpdateView(FormView):
         return redirect(self.success_url)
 
 
-class KurikulumReadSyncView(SessionWizardView):
+class KurikulumReadSyncView(MySessionWizardView):
     """Kurikulum synchronization from Neosia
     What to do in sync:
     1. Create Mata Kuliah Kurikulum
@@ -333,8 +283,6 @@ class KurikulumReadSyncView(SessionWizardView):
     """
     template_name: str = 'kurikulum/read-all-sync-form.html'
     form_list: list = [SemesterFromNeosia, BulkUpdateSemester]
-    latest_page: str = '0'
-    revealed_page: list = []
 
 
 class KurikulumReadAllView(ListView):
