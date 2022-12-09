50d26c698dda2643c6f031a973234664f522ccec
Alfian Aldy Hamdani <alfian.hamdani04@gmail.com>
2022-12-09 22:37:52 +0800

    Add delete form and make form in formset required

--- static/public/js/formset.js
+++ static/public/js/formset.js
@@ -1,10 +1,12 @@
 function addMoreForm(totalFormId) {
-    $('#add-form').on('click', function(e){
+    $('#add-form').on('click', function(){
         const currentFormCount = $('.added-form').length; // + 1
         const copyEmptyFormEl = $('#empty-form').clone(true);
 
         copyEmptyFormEl.removeClass('hidden').addClass('added-form mb-3');
         copyEmptyFormEl.attr('id', `form-${currentFormCount}`);
+        
+        copyEmptyFormEl.find('.form-control').attr('required', 'true');
         
         const regex = new RegExp('__prefix__', 'g');
         copyEmptyFormEl.html(copyEmptyFormEl.html().replace(regex, currentFormCount));
@@ -13,4 +15,14 @@ function addMoreForm(totalFormId) {
         // now add new empty form element to our html form
         $('#form-list').append(copyEmptyFormEl);
     });
+}
+
+function deleteForm(button, totalFormId) {
+    // Delete form
+    const formNumber = $(button).attr('data-delete');
+    $(`#form-${formNumber}`).remove();
+
+    // Update total form
+    const currentFormCount = $('.added-form').length;
+    $(totalFormId).val(currentFormCount);
 }
