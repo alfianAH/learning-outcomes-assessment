4220a33f81e1f57e4635a58d7e4c513f88385022
Alfian Aldy Hamdani <alfian.hamdani04@gmail.com>
2022-12-23 11:20:28 +0800

    Add models for mata kuliah semester

--- ilo/models.py
+++ ilo/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models import CheckConstraint, Q
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
 from pi_area.models import PerformanceIndicatorArea
 
@@ -8,8 +10,22 @@ class Ilo(models.Model):
 
     nama = models.CharField(max_length=255, null=False)
     deskripsi = models.TextField(null=False)
-    satisfactory_level = models.FloatField(null=False)
-    persentase_capaian_ilo = models.FloatField(null=True)
+    satisfactory_level = models.FloatField(null=False,
+        validators=[MinValueValidator(0.0), MaxValueValidator(100.0)])
+    persentase_capaian_ilo = models.FloatField(null=True,
+        validators=[MinValueValidator(0.0), MaxValueValidator(100.0)])
+
+    class Meta:
+        constraints = (
+            CheckConstraint(
+                check=Q(satisfactory_level__gte=0.0) & Q(satisfactory_level__lte=100.0),
+                name='satisfactory_level_range'
+            ),
+            CheckConstraint(
+                check=Q(persentase_capaian_ilo__gte=0.0) & Q(persentase_capaian_ilo__lte=100.0),
+                name='persentase_capaian_ilo_range'
+            ),
+        )
 
     def get_semester(self):
         return self.pi_area.assessment_area.semester
--- mata_kuliah/models.py
+++ mata_kuliah/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django.db.models import CheckConstraint, Q
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
-
+from django.conf import settings
 from accounts.models import ProgramStudi
+from ilo.models import Ilo
 from kurikulum.models import Kurikulum
+from semester.models import SemesterKurikulum
+
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
 class MataKuliahKurikulum(models.Model):
@@ -27,3 +33,58 @@ class MataKuliahKurikulum(models.Model):
             'kurikulum_id': self.kurikulum.id_neosia,
             'mk_id': self.id_neosia
         })
+
+
+class MataKuliahSemester(models.Model):
+    mk_kurikulum = models.ForeignKey(MataKuliahKurikulum, on_delete=models.CASCADE)
+    semester = models.ForeignKey(SemesterKurikulum, on_delete=models.CASCADE)
+
+    average_clo_achievement = models.FloatField(null=True, 
+        validators=[MinValueValidator(0.0), MaxValueValidator(100.0)])
+
+    class Meta:
+        constraints = (
+            CheckConstraint(
+                check=Q(average_clo_achievement__gte=0.0) & Q(average_clo_achievement__lte=100.0),
+                name='average_clo_achievement_range'
+            ),
+        )
+
+
+class PesertaMataKuliah(models.Model):
+    mk_semester = models.ForeignKey(MataKuliahSemester, on_delete=models.CASCADE)
+    mahasiswa = models.ForeignKey(User, on_delete=models.CASCADE)
+    kelas = models.CharField(max_length=20)
+
+
+class DosenMataKuliah(models.Model):
+    mk_semester = models.ForeignKey(MataKuliahSemester, on_delete=models.CASCADE)
+    mahasiswa = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class NilaiMataKuliahMahasiswa(models.Model):
+    peserta = models.ForeignKey(PesertaMataKuliah, on_delete=models.CASCADE)
+    nilai_akhir = models.FloatField(null=True, 
+        validators=[MinValueValidator(0.0), MaxValueValidator(100.0)])
+    
+    class Meta:
+        constraints = (
+            CheckConstraint(
+                check=Q(nilai_akhir__gte=0.0) & Q(nilai_akhir__lte=100.0),
+                name='nilai_akhir_range'
+            ),
+        ) 
+
+
+class NilaiMataKuliahIloMahasiswa(models.Model):
+    peserta = models.ForeignKey(PesertaMataKuliah, on_delete=models.CASCADE)
+    ilo = models.ForeignKey(Ilo, on_delete=models.CASCADE)
+    nilai_ilo = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(100.0)])
+
+    class Meta:
+        constraints = (
+            CheckConstraint(
+                check=Q(nilai_ilo__gte=0.0) & Q(nilai_ilo__lte=100.0),
+                name='nilai_ilo_range'
+            ),
+        ) 
