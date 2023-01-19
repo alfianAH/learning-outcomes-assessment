b4170d131f71dd0e832317bc240c6497f0ffd4f0
Alfian Aldy Hamdani <alfian.hamdani04@gmail.com>
2023-01-19 16:54:27 +0800

    Add confirmation before deleting something

--- static/public/js/loading.js
+++ static/public/js/loading.js
@@ -1,12 +1,16 @@
 function showLoadingIndicator() {
-    $('.loading-trigger').on('click', function () {
-        // Showing loading card
-        $('#loading-card').addClass('showing');
+    // Showing loading card
+    $('#loading-card').addClass('showing');
         
-        // Show loading card
-        $('#loading-card').one(transitionEvent, function () {
-            $('#loading-card').removeClass('showing').addClass('show');
-        });
+    // Show loading card
+    $('#loading-card').one(transitionEvent, function () {
+        $('#loading-card').removeClass('showing').addClass('show');
+    });
+}
+
+function loadingIndicatorListener() {
+    $('.loading-trigger').on('click', function () {
+        showLoadingIndicator();
     });
 }
 
@@ -20,8 +24,26 @@ function hideLoadingIndicator() {
     });
 }
 
+function confirmDelete() {
+    $('.confirmation').on('click', function () {
+        let confirmMessage = $(this).attr('data-confirm');
+        
+        if (confirmMessage == null) {
+            confirmMessage = 'Apakah anda yakin?'
+        }
+        
+        if (confirm(confirmMessage)) {
+            showLoadingIndicator();
+            return true;
+        } else {
+            return false;
+        }
+    });
+}
+
 window.onunload = function () {
     hideLoadingIndicator();
 }
 
-showLoadingIndicator();
+confirmDelete();
+loadingIndicatorListener();
