962163417175faa3d2b8d5286d2fb3352a14c5ed
Alfian Aldy Hamdani <alfian.hamdani04@gmail.com>
2023-02-28 17:22:09 +0800

    Use get_reverse_url for reverse url

--- kurikulum/models.py
+++ kurikulum/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.urls import reverse
-
+from learning_outcomes_assessment.utils import get_reverse_url
 from accounts.models import ProgramStudiJenjang
 
 
@@ -12,86 +11,64 @@ class Kurikulum(models.Model):
     tahun_mulai = models.IntegerField(null=False)
     is_active = models.BooleanField(null=False)
 
+    is_assessmentarea_locked = models.BooleanField(default=False)
+
     def __str__(self) -> str:
         return self.nama
+    
+    @property
+    def get_kwargs(self):
+        return {
+            'kurikulum_id': self.pk
+        }
 
     def read_sync_url(self):
-        return reverse('kurikulum:read-sync', kwargs={
-            'kurikulum_id': self.id_neosia
-        })
+        return get_reverse_url('kurikulum:read-sync', self.get_kwargs)
 
     def read_detail_url(self):
-        return reverse('kurikulum:read', kwargs={
-            'kurikulum_id': self.id_neosia
-        })
+        return get_reverse_url('kurikulum:read', self.get_kwargs)
 
     # Mata Kuliah Kurikulum
     def get_mk_kurikulum(self):
         return self.matakuliahkurikulum_set.all()
     
     def read_all_mk_kurikulum_url(self):
-        return reverse('kurikulum:mata_kuliah_kurikulum:read-all', kwargs={
-            'kurikulum_id': self.id_neosia
-        })
+        return get_reverse_url('kurikulum:mata_kuliah_kurikulum:read-all', self.get_kwargs)
 
     def get_bulk_delete_mk_kurikulum_url(self):
-        return reverse('kurikulum:mata_kuliah_kurikulum:bulk-delete', kwargs={
-            'kurikulum_id': self.id_neosia
-        })
+        return get_reverse_url('kurikulum:mata_kuliah_kurikulum:bulk-delete', self.get_kwargs)
     
     def get_create_mk_kurikulum_url(self):
-        return reverse('kurikulum:mata_kuliah_kurikulum:create', kwargs={
-            'kurikulum_id': self.id_neosia
-        })
+        return get_reverse_url('kurikulum:mata_kuliah_kurikulum:create', self.get_kwargs)
 
     def get_bulk_update_mk_kurikulum_url(self):
-        return reverse('kurikulum:mata_kuliah_kurikulum:bulk-update', kwargs={
-            'kurikulum_id': self.id_neosia
-        })
+        return get_reverse_url('kurikulum:mata_kuliah_kurikulum:bulk-update', self.get_kwargs)
 
     # Performance Indicator Area
     def read_all_pi_area_url(self):
-        return reverse('kurikulum:pi_area:read-all', kwargs={
-            'kurikulum_id': self.id_neosia
-        })
+        return get_reverse_url('kurikulum:pi_area:read-all', self.get_kwargs)
 
     def get_hx_create_pi_area_url(self):
-        return reverse('kurikulum:pi_area:hx-create', kwargs={
-            'kurikulum_id': self.id_neosia
-        })
+        return get_reverse_url('kurikulum:pi_area:hx-create', self.get_kwargs)
 
     def get_create_pi_area_url(self):
-        return reverse('kurikulum:pi_area:create', kwargs={
-            'kurikulum_id': self.id_neosia
-        })
+        return get_reverse_url('kurikulum:pi_area:create', self.get_kwargs)
 
     def get_bulk_delete_pi_area_url(self):
-        return reverse('kurikulum:pi_area:pi-area-bulk-delete', kwargs={
-            'kurikulum_id': self.id_neosia
-        })
+        return get_reverse_url('kurikulum:pi_area:pi-area-bulk-delete', self.get_kwargs)
 
     def get_duplicate_pi_area_url(self):
-        return reverse('kurikulum:pi_area:duplicate', kwargs={
-            'kurikulum_id': self.id_neosia
-        })
+        return get_reverse_url('kurikulum:pi_area:duplicate', self.get_kwargs)
 
     # ILO
     def read_all_ilo_url(self):
-        return reverse('kurikulum:ilo:read-all', kwargs={
-            'kurikulum_id': self.id_neosia
-        })
+        return get_reverse_url('kurikulum:ilo:read-all', self.get_kwargs)
 
     def get_hx_create_ilo_url(self):
-        return reverse('kurikulum:ilo:hx-create', kwargs={
-            'kurikulum_id': self.id_neosia
-        })
+        return get_reverse_url('kurikulum:ilo:hx-create', self.get_kwargs)
 
     def get_create_ilo_url(self):
-        return reverse('kurikulum:ilo:create', kwargs={
-            'kurikulum_id': self.id_neosia
-        })
+        return get_reverse_url('kurikulum:ilo:create', self.get_kwargs)
 
     def get_ilo_bulk_delete_url(self):
-        return reverse('kurikulum:ilo:bulk-delete', kwargs={
-            'kurikulum_id': self.id_neosia
-        })
+        return get_reverse_url('kurikulum:ilo:bulk-delete', self.get_kwargs)
--- pi_area/models.py
+++ pi_area/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-from django.urls import reverse
+from learning_outcomes_assessment.utils import get_reverse_url
 from kurikulum.models import Kurikulum
+from lock_model.models import LockableMixin
+
 
 # Create your models here.
 class ColorChoices(models.TextChoices):
@@ -13,7 +15,7 @@ class ColorChoices(models.TextChoices):
     PURPLE = 'Ungu', 'badge-custom-purple'
 
 
-class AssessmentArea(models.Model):
+class AssessmentArea(LockableMixin, models.Model):
     kurikulum = models.ForeignKey(Kurikulum, on_delete=models.CASCADE)
     nama = models.CharField(max_length=100, null=False, blank=False)
     color = models.CharField(max_length=7, choices=ColorChoices.choices, null=False, blank=False, default=ColorChoices.DEFAULT)
@@ -23,35 +25,28 @@ class AssessmentArea(models.Model):
 
     def __str__(self) -> str:
         return self.nama
+    
+    @property
+    def get_kwargs(self):
+        return {
+            **self.kurikulum.get_kwargs,
+            'assessment_area_id': self.pk
+        }
 
     def get_pi_area(self):
         return self.performanceindicatorarea_set.all()
 
     def get_hx_update_pi_area_url(self):
-        return reverse('kurikulum:pi_area:hx-update', kwargs={
-            'kurikulum_id': self.kurikulum.id_neosia,
-            'assessment_area_id': self.pk
-        })
+        return get_reverse_url('kurikulum:pi_area:hx-update', self.get_kwargs)
 
     def get_update_pi_area_url(self):
-        return reverse('kurikulum:pi_area:update', kwargs={
-            'kurikulum_id': self.kurikulum.id_neosia,
-            'assessment_area_id': self.pk
-        })
-    
-    def get_read_all_pi_area_url(self):
-        return reverse('kurikulum:pi_area:read-all', kwargs={
-            'kurikulum_id': self.kurikulum.id_neosia
-        })
+        return get_reverse_url('kurikulum:pi_area:update', self.get_kwargs)
 
     def get_delete_assessment_area_url(self):
-        return reverse('kurikulum:pi_area:assessment-area-delete', kwargs={
-            'kurikulum_id': self.kurikulum.id_neosia,
-            'assessment_area_id': self.pk
-        })
+        return get_reverse_url('kurikulum:pi_area:assessment-area-delete', self.get_kwargs)
 
 
-class PerformanceIndicatorArea(models.Model):
+class PerformanceIndicatorArea(LockableMixin, models.Model):
     assessment_area = models.ForeignKey(AssessmentArea, on_delete=models.CASCADE)
     pi_code = models.CharField(max_length=20, null=False, blank=False)
 
@@ -61,22 +56,23 @@ class PerformanceIndicatorArea(models.Model):
     def __str__(self) -> str:
         return self.pi_code
     
+    @property
+    def get_kwargs(self):
+        return {
+            **self.assessment_area.kurikulum.get_kwargs,
+            'pi_area_id': self.pk
+        }
+    
     def get_performance_indicator(self):
         return self.performanceindicator_set.all()
 
     def read_detail_url(self):
-        return reverse('kurikulum:pi_area:pi-area-read', kwargs={
-            'kurikulum_id': self.assessment_area.kurikulum.id_neosia,
-            'pi_area_id': self.pk
-        })
+        return get_reverse_url('kurikulum:pi_area:pi-area-read',  self.get_kwargs)
     
     def get_pi_area_update_url(self):
-        return reverse('kurikulum:pi_area:pi-area-update', kwargs={
-            'kurikulum_id': self.assessment_area.kurikulum.id_neosia,
-            'pi_area_id': self.pk
-        })
+        return get_reverse_url('kurikulum:pi_area:pi-area-update',  self.get_kwargs)
 
 
-class PerformanceIndicator(models.Model):
+class PerformanceIndicator(LockableMixin, models.Model):
     pi_area = models.ForeignKey(PerformanceIndicatorArea, on_delete=models.CASCADE)
     deskripsi = models.TextField()
