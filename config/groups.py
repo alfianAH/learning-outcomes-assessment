b7db993b5df7c56c76e9026b3c8975b25160d872
Alfian Aldy Hamdani <alfian.hamdani04@gmail.com>
2023-04-24 12:13:46 +0800

    Set permissions to group

--- config/groups.py
+++ config/groups.py
@@ -5,123 +5,297 @@ dosen_group, dosen_created = Group.objects.get_or_create(name='Dosen')
 mahasiswa_group, mahasiswa_created = Group.objects.get_or_create(name='Mahasiswa')
 
 admin_prodi_permissions = [
-    # Kurikulum
-    'add_kurikulum',
-    'change_kurikulum',
-    'delete_kurikulum',
-    'view_kurikulum',
+    'add_fakultas',
+    'change_fakultas',
+    'delete_fakultas',
+    'view_fakultas',
 
-    # Mata kuliah kurikulum
-    'add_matakuliahkurikulum',
-    'change_matakuliahkurikulum',
-    'delete_matakuliahkurikulum',
-    'view_matakuliahkurikulum',
+    'add_jenjangstudi',
+    'change_jenjangstudi',
+    'delete_jenjangstudi',
+    'view_jenjangstudi',
 
-    # Assessment Area
-    'add_assessmentarea',
-    'change_assessmentarea',
-    'delete_assessmentarea',
-    'view_assessmentarea',
+    'add_myuser',
+    'change_myuser',
+    'delete_myuser',
+    'view_myuser',
 
-    # Performance Indicator Area
-    'add_performanceindicatorarea',
-    'change_performanceindicatorarea',
-    'delete_performanceindicatorarea',
-    'view_performanceindicatorarea',
+    'add_programstudi',
+    'change_programstudi',
+    'delete_programstudi',
+    'view_programstudi',
 
-    # Performance Indicator
-    'add_performanceindicator',
-    'change_performanceindicator',
-    'delete_performanceindicator',
-    'view_performanceindicator',
+    'add_programstudijenjang',
+    'change_programstudijenjang',
+    'delete_programstudijenjang',
+    'view_programstudijenjang',
 
-    # ILO
+    'add_clo',
+    'change_clo',
+    'delete_clo',
+    'view_clo',
+
+    'add_komponenclo',
+    'change_komponenclo',
+    'delete_komponenclo',
+    'view_komponenclo',
+
+    'add_nilaiclomatakuliahsemester',
+    'change_nilaiclomatakuliahsemester',
+    'delete_nilaiclomatakuliahsemester',
+    'view_nilaiclomatakuliahsemester',
+
+    'add_nilaiclopeserta',
+    'change_nilaiclopeserta',
+    'delete_nilaiclopeserta',
+    'view_nilaiclopeserta',
+
+    'add_nilaikomponenclopeserta',
+    'change_nilaikomponenclopeserta',
+    'delete_nilaikomponenclopeserta',
+    'view_nilaikomponenclopeserta',
+
+    'add_piclo',
+    'change_piclo',
+    'delete_piclo',
+    'view_piclo',
+
     'add_ilo',
     'change_ilo',
     'delete_ilo',
     'view_ilo',
 
-    # Mata Kuliah Semester
-    'add_matakuliahsemester',
-    'change_matakuliahsemester',
-    'delete_matakuliahsemester',
-    'view_matakuliahsemester',
+    'add_kurikulum',
+    'change_kurikulum',
+    'delete_kurikulum',
+    'view_kurikulum',
 
-    # Kelas Mata Kuliah Semester
+    'add_matakuliahkurikulum',
+    'change_matakuliahkurikulum',
+    'delete_matakuliahkurikulum',
+    'view_matakuliahkurikulum',
+
+    'add_lock',
+    'change_lock',
+    'delete_lock',
+    'view_lock',
+
+    'add_dosenmatakuliah',
+    'change_dosenmatakuliah',
+    'delete_dosenmatakuliah',
+    'view_dosenmatakuliah',
+
     'add_kelasmatakuliahsemester',
     'change_kelasmatakuliahsemester',
     'delete_kelasmatakuliahsemester',
     'view_kelasmatakuliahsemester',
 
-    # Dosen Mata Kuliah
-    'add_dosenmatakuliah',
-    'change_dosenmatakuliah',
-    'delete_dosenmatakuliah',
-    'view_dosenmatakuliah',
+    'add_matakuliahsemester',
+    'change_matakuliahsemester',
+    'delete_matakuliahsemester',
+    'view_matakuliahsemester',
+
+    'add_nilaiexcelmatakuliahsemester',
+    'change_nilaiexcelmatakuliahsemester',
+    'delete_nilaiexcelmatakuliahsemester',
+    'view_nilaiexcelmatakuliahsemester',
+
+    'add_nilaimatakuliahilomahasiswa',
+    'change_nilaimatakuliahilomahasiswa',
+    'delete_nilaimatakuliahilomahasiswa',
+    'view_nilaimatakuliahilomahasiswa',
+
+    'add_pesertamatakuliah',
+    'change_pesertamatakuliah',
+    'delete_pesertamatakuliah',
+    'view_pesertamatakuliah',
+
+    'add_performanceindicator',
+    'change_performanceindicator',
+    'delete_performanceindicator',
+    'view_performanceindicator',
+
+    'add_assessmentarea',
+    'change_assessmentarea',
+    'delete_assessmentarea',
+    'view_assessmentarea',
+
+    'add_performanceindicatorarea',
+    'change_performanceindicatorarea',
+    'delete_performanceindicatorarea',
+    'view_performanceindicatorarea',
+
+    'add_rencanapembelajaransemester',
+    'change_rencanapembelajaransemester',
+    'delete_rencanapembelajaransemester',
+    'view_rencanapembelajaransemester',
+
+    'add_semester',
+    'change_semester',
+    'delete_semester',
+    'view_semester',
+
+    'add_semesterprodi',
+    'change_semesterprodi',
+    'delete_semesterprodi',
+    'view_semesterprodi',
+
+    'add_tahunajaran',
+    'change_tahunajaran',
+    'delete_tahunajaran',
+    'view_tahunajaran',
+
+    'add_tahunajaranprodi',
+    'change_tahunajaranprodi',
+    'delete_tahunajaranprodi',
+    'view_tahunajaranprodi',
 ]
 
 dosen_permissions = [
-    # Kurikulum
-    'add_kurikulum',
-    'change_kurikulum',
-    'delete_kurikulum',
+    'view_programstudijenjang',
+
+    'add_clo',
+    'change_clo',
+    'delete_clo',
+    'view_clo',
+
+    'add_komponenclo',
+    'change_komponenclo',
+    'delete_komponenclo',
+    'view_komponenclo',
+
+    'add_nilaiclomatakuliahsemester',
+    'change_nilaiclomatakuliahsemester',
+    'delete_nilaiclomatakuliahsemester',
+    'view_nilaiclomatakuliahsemester',
+
+    'add_nilaiclopeserta',
+    'change_nilaiclopeserta',
+    'delete_nilaiclopeserta',
+    'view_nilaiclopeserta',
+
+    'add_nilaikomponenclopeserta',
+    'change_nilaikomponenclopeserta',
+    'delete_nilaikomponenclopeserta',
+    'view_nilaikomponenclopeserta',
+
+    'add_piclo',
+    'change_piclo',
+    'delete_piclo',
+    'view_piclo',
+
+    'add_ilo',
+    'change_ilo',
+    'delete_ilo',
+    'view_ilo',
+    
     'view_kurikulum',
 
-    # Mata kuliah kurikulum
     'add_matakuliahkurikulum',
     'change_matakuliahkurikulum',
     'delete_matakuliahkurikulum',
     'view_matakuliahkurikulum',
 
-    # Assessment Area
-    'add_assessmentarea',
-    'change_assessmentarea',
-    'delete_assessmentarea',
-    'view_assessmentarea',
+    'add_lock',
+    'change_lock',
+    'delete_lock',
+    'view_lock',
 
-    # Performance Indicator Area
-    'add_performanceindicatorarea',
-    'change_performanceindicatorarea',
-    'delete_performanceindicatorarea',
-    'view_performanceindicatorarea',
+    'add_dosenmatakuliah',
+    'change_dosenmatakuliah',
+    'delete_dosenmatakuliah',
+    'view_dosenmatakuliah',
 
-    # Performance Indicator
-    'add_performanceindicator',
-    'change_performanceindicator',
-    'delete_performanceindicator',
-    'view_performanceindicator',
-
-    # ILO
-    'add_ilo',
-    'change_ilo',
-    'delete_ilo',
-    'view_ilo',
-
-    # Mata Kuliah Semester
-    'add_matakuliahsemester',
-    'change_matakuliahsemester',
-    'delete_matakuliahsemester',
-    'view_matakuliahsemester',
-
-    # Kelas Mata Kuliah Semester
     'add_kelasmatakuliahsemester',
     'change_kelasmatakuliahsemester',
     'delete_kelasmatakuliahsemester',
     'view_kelasmatakuliahsemester',
 
-    # Dosen Mata Kuliah
-    'add_dosenmatakuliah',
-    'change_dosenmatakuliah',
-    'delete_dosenmatakuliah',
-    'view_dosenmatakuliah',
+    'add_matakuliahsemester',
+    'change_matakuliahsemester',
+    'delete_matakuliahsemester',
+    'view_matakuliahsemester',
+
+    'add_nilaiexcelmatakuliahsemester',
+    'change_nilaiexcelmatakuliahsemester',
+    'delete_nilaiexcelmatakuliahsemester',
+    'view_nilaiexcelmatakuliahsemester',
+
+    'add_nilaimatakuliahilomahasiswa',
+    'change_nilaimatakuliahilomahasiswa',
+    'delete_nilaimatakuliahilomahasiswa',
+    'view_nilaimatakuliahilomahasiswa',
+
+    'add_pesertamatakuliah',
+    'change_pesertamatakuliah',
+    'delete_pesertamatakuliah',
+    'view_pesertamatakuliah',
+
+    'add_performanceindicator',
+    'change_performanceindicator',
+    'delete_performanceindicator',
+    'view_performanceindicator',
+
+    'add_assessmentarea',
+    'change_assessmentarea',
+    'delete_assessmentarea',
+    'view_assessmentarea',
+
+    'add_performanceindicatorarea',
+    'change_performanceindicatorarea',
+    'delete_performanceindicatorarea',
+    'view_performanceindicatorarea',
+
+    'add_rencanapembelajaransemester',
+    'change_rencanapembelajaransemester',
+    'delete_rencanapembelajaransemester',
+    'view_rencanapembelajaransemester',
+
+    'view_semester',
+    
+    'view_semesterprodi',
+    
+    'view_tahunajaran',
+
+    'view_tahunajaranprodi',
 ]
 
 mahasiswa_permissions = [
+    'view_clo',
+    'view_komponenclo',
+    'view_nilaiclopeserta',
+    'view_nilaikomponenclopeserta',
+    'view_piclo',
+    'view_ilo',
+    'view_kurikulum',
+    'view_matakuliahkurikulum',
+    'view_lock',
+    'view_dosenmatakuliah',
+    'view_kelasmatakuliahsemester',
+    'view_matakuliahsemester',
+    'view_nilaimatakuliahilomahasiswa',
+    'view_pesertamatakuliah',
+    'view_performanceindicator',
+    'view_assessmentarea',
+    'view_performanceindicatorarea',
+    'view_rencanapembelajaransemester',
+    'view_semester',
+    'view_semesterprodi',
+    'view_tahunajaran',
+    'view_tahunajaranprodi',
 ]
 
 def set_permissions(group, permissions):
     for permission in permissions:
-        permission = Permission.objects.get(codename=permission)
+        try:
+            permission = Permission.objects.get(codename=permission)
+        except Permission.MultipleObjectsReturned:
+            print('Multi object found: {}'.format(permission))
+            permission = Permission.objects.filter(codename=permission).first()
+        except Permission.DoesNotExist:
+            print('Does not exist: {}'.format(permission))
+            continue
+        
         group.permissions.add(permission)
 
 
