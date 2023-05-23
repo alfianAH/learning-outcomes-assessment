d9a06c5211d0fa2fcb0c99c170e20efc06828cd4
Alfian Aldy Hamdani <alfian.hamdani04@gmail.com>
2023-05-24 00:29:49 +0800

    Add static files url in urls.py

--- learning_outcomes_assessment/urls.py
+++ learning_outcomes_assessment/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from .views import home_view, dosen_json_response
+from django.views.static import serve 
+from django.conf import settings
 
 
 urlpatterns = [
@@ -26,4 +28,6 @@ urlpatterns = [
     path('semester/', include('semester.urls')),
     path('laporan-cpl/', include('laporan_cpl.urls')),
     path('dosen-search/', dosen_json_response, name='dosen-search'),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
